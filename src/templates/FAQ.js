9ea20864381d97d7b766e98736a93dfbbc92e069
Lane Le Prevost-Smith <lanelps@gmail.com>
2021-12-01 16:55:04 +1300

    faq schema

---
+++ src/templates/FAQ.js
@@ -0,0 +1,66 @@
+import { fetchConfig } from '../utils/helpers';
+
+const FAQ = () => {
+  if (typeof window === `undefined`) {
+    return;
+  }
+
+  // ---------------------------------------------------------------------------
+  // DOM
+
+  const buttons = document.querySelectorAll(`.faq__button`);
+  const faqContent = document.querySelectorAll(`.faq-content`);
+
+  //
+
+  // ---------------------------------------------------------------------------
+  // variables
+
+  // ---------------------------------------------------------------------------
+  // methods
+
+  const resetStyles = () => {
+    buttons.forEach(button => {
+      button.classList.remove(`selected`);
+    });
+
+    faqContent.forEach((contentItem, contentIndex) => {
+      contentItem.classList.remove(`active`);
+      contentItem.classList.add(`hidden`);
+    });
+  };
+
+  // ---------------------------------------------------------------------------
+  // initialization
+
+  const addListeners = () => {
+    if (buttons?.[0]) {
+      buttons.forEach(button => {
+        button.addEventListener(`click`, e => {
+          resetStyles();
+
+          button.classList.add(`selected`);
+
+          const index = button.getAttribute(`data-index`);
+
+          faqContent[index].classList.remove(`hidden`);
+
+          setTimeout(() => {
+            faqContent[index].classList.add(`active`);
+          }, 10);
+        });
+      });
+    }
+  };
+
+  // ---------------------------------------------------------------------------
+  // execution
+
+  const main = () => {
+    addListeners();
+  };
+
+  main();
+};
+
+export default FAQ;
